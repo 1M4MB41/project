7d783b7a87ed40554d21484467f0620cd35dddbf
Matvey <1m4mb41@gmail.com>
2023-08-08 22:55:37 +0300

    first commit

---
+++ JS.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+// const result = confirm("Are you here?");
+// alert('Hello');
+// alert(result);
+// const answer = prompt("Вам есть 18?", "Да");
+// alert(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", '');
+// answers[1] = prompt("Как ваше фамилия?", '');
+// answers[2] = prompt("Сколько вам лет?", '');
+
+// console.log(typeof (answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}`);
+
+// const user = 'Ivan';
+// alert(`Hi, ${user}`);
+// console.log('4' + " - object");
+
+let incr = 10;
+let decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 / 2);
+console.log(2 + 2 * 2 == 8);
+
+const isChecked = true,
+  isClosed = false;
+console.log(isChecked || isClosed);
